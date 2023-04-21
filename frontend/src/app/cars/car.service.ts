8e4cf84ce5b19398bf0111d39ea465b8cb3b1f30
Uka0001 <ukraine0001@gmail.com>
2023-04-21 10:12:45 +0300

    get cars on angular completed

--- frontend/src/app/cars/car.service.ts
+++ frontend/src/app/cars/car.service.ts
@@ -2,14 +2,15 @@ import { Injectable } from '@angular/core';
 import {Observable} from "rxjs";
 import {Car} from "../model/car";
 import {HttpClient} from "@angular/common/http";
-import {environment} from "../../environments/environment";
+// import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarService {
-  private readonly apiUrl = environment.apiUrl;
-  private readonly carPrefix = '/cars'
+  // private readonly apiUrl = environment.apiUrl;
+  private readonly apiUrl = 'http://localhost:8080';
+  private readonly carPrefix = '/cars';
 
   constructor(private httpClient: HttpClient) { }
 
